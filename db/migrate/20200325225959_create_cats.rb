4b1f3fbf76e3d32d6e92ed5a7bf021d72d76ac11
Ubuntu <ubuntu@ip-172-31-27-127.us-east-2.compute.internal>
2020-03-25 23:25:44 +0000

    seeded

--- db/migrate/20200325225959_create_cats.rb
+++ db/migrate/20200325225959_create_cats.rb
@@ -1,12 +1,2 @@
 class CreateCats < ActiveRecord::Migration[6.0]
-  def change
-    create_table :cats do |t|
-      t.string :name
-      t.string :age
-      t.string :image
-      t.string :interests
-
-      t.timestamps
-    end
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cats = [
+  {
+    name: "Whirlpool",
+    age: "3",
+    image:
+      "https://i2.pickpik.com/photos/370/957/85/cat-mieze-tiger-cat-funny-preview.jpg",
+    interests: "knocking stuff off the countertop"
+  },
+  {
+    name: "BeanyWeeny",
+    age: "1",
+    image: "https://live.staticflickr.com/1677/25732632800_d97d5fe8d8.jpg",
+    interests: "drinking out of the toilet"
+  },
+  {
+    name: "GorillaGlue",
+    age: "3",
+    image:
+      "https://c1.wallpaperflare.com/preview/250/604/965/persian-cat-cat-felidae-domestic-cat.jpg",
+    interests: "sleeping on keyboards"
+  },
+  {
+    name: "OrangeHaze",
+    age: "1",
+    image:
+      "https://p2.piqsels.com/preview/96/218/548/cat-domestic-cat-mackerel-pet.jpg",
+    interests: "watching romcoms"
+  },
+  {
+    name: "Smitty WerbenJagerManJensen",
+    age: "1",
+    image:
+      "https://images.dailykos.com/images/295092/large/Taco-Cat-Spelled-Backwards-Is-Taco-Cat.-e1430184838220_1_.jpg?1472864097",
+    interests: "Being Number 1"
+  },
+  {
+    name: "Richard",
+    age: "6",
+    image:
+      "https://i.pinimg.com/originals/72/48/a1/7248a1d4a343b9e78930bf250a2db212.jpg",
+    interests: "Being not suspicious at all"
+  }
+];
+
+cats.each do |cat|
+    Cat.create cat
+end
